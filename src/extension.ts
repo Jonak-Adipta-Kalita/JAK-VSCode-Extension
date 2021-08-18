94f81ad56213201681944414e8db7bfcaa6a81eb
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-08-18 09:58:34 +0530

    implemented prettier and eslint

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,19 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "jak-vscode-extension" is now active!');
-	let disposable = vscode.commands.registerCommand('jak-vscode-extension.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from jak-vscode-extension!');
-	});
+    let disposable = vscode.commands.registerCommand(
+        "jak-vscode-extension.openJAKWebsite",
+        () => {
+            vscode.window.showInformationMessage(
+                "Opening JAK's Website in the Web Browser!!"
+            );
+            vscode.env.openExternal(vscode.Uri.parse(
+                "https://jonakadiptakalita.herokuapp.com"
+            ));
+        }
+    );
 
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
