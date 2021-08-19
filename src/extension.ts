8c11439ecea6e9e6ef2868e176056968f9203d85
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-08-19 13:47:31 +0530

    dont want to write vscode.something again and again

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
-import * as vscode from "vscode";
+import { ExtensionContext } from "vscode";
 import { openJAKWebsite, openJAKAPI, openJAKDiscordBot } from "./functions";
 
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
     context.subscriptions.push(openJAKWebsite, openJAKAPI, openJAKDiscordBot);
 }
--- src/functions.ts
+++ src/functions.ts
@@ -1,37 +1,31 @@
-import * as vscode from "vscode";
+import { commands, env, Uri, window } from "vscode";
 
-const openJAKWebsite = vscode.commands.registerCommand(
+const openJAKWebsite = commands.registerCommand(
     "jak-vscode-extension.openJAKWebsite",
     () => {
-        vscode.window.showInformationMessage(
+        window.showInformationMessage(
             "Opening JAK's Website in the Web Browser!!"
         );
-        vscode.env.openExternal(
-            vscode.Uri.parse("https://jonakadiptakalita.herokuapp.com/")
-        );
+        env.openExternal(Uri.parse("https://jonakadiptakalita.herokuapp.com/"));
     }
 );
 
-const openJAKAPI = vscode.commands.registerCommand(
+const openJAKAPI = commands.registerCommand(
     "jak-vscode-extension.openJAKAPI",
     () => {
-        vscode.window.showInformationMessage(
-            "Opening JAK's API in the Web Browser"
-        );
-        vscode.env.openExternal(
-            vscode.Uri.parse("https://jak-api-dot-com.herokuapp.com/")
-        );
+        window.showInformationMessage("Opening JAK's API in the Web Browser");
+        env.openExternal(Uri.parse("https://jak-api-dot-com.herokuapp.com/"));
     }
 );
 
-const openJAKDiscordBot = vscode.commands.registerCommand(
+const openJAKDiscordBot = commands.registerCommand(
     "jak-vscode-extension.openJAKDiscordBot",
     () => {
-        vscode.window.showInformationMessage(
+        window.showInformationMessage(
             "Open JAK's Discord Bot Invite Link in the Web Browser"
         );
-        vscode.env.openExternal(
-            vscode.Uri.parse(
+        env.openExternal(
+            Uri.parse(
                 "https://discord.com/oauth2/authorize?client_id=756402881913028689&scope=bot"
             )
         );
