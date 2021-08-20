4dcad3eda988529c7ca8125df7d35c74d3394a84
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-08-20 15:00:30 +0530

    made the sidebar

--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -1,4 +1,56 @@
 <script lang="ts"></script>
-<style></style>
+<style>
+    .header h3 {
+        display: flex;
+        justify-content: center;
+        font-weight: bold;
+    }
 
-<div></div>
+    .body {
+        margin-top: 20px;
+    }
+
+    .body h4 {
+        display: flex;
+        margin-left: 20px;
+        font-weight: bold;
+    }
+
+    .body div {
+        margin-left: 10px;
+    }
+
+    .body div p {
+        margin-left: 25px;
+        margin-top: 10px;
+    }
+
+    .footer {
+        margin-top: 20px;
+        padding: 5px;
+    }
+
+    .footer h6 {
+        display: flex;
+        justify-content: center;
+        font-weight: bold;
+    }
+</style>
+
+<div>
+    <div class="header">
+        <h3>Made by JAK</h3>
+    </div>
+    <div class="body">
+        <h4>Available Commands:</h4>
+        <div>
+            <p>1) Refresh JAK VSCode Extension</p>
+            <p>2) Open JAK's Website</p>
+            <p>3) Open JAK's API</p>
+            <p>4) Open JAK's Discord Invite</p>
+        </div>
+    </div>
+    <div class="footer">
+        <h6>&copy; 2021</h6>
+    </div>
+</div>
